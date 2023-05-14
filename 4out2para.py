2a1f63beceb9f0215e1c89e6880518d575c2e086
Bipul Sharma <106522978+Princessshark@users.noreply.github.com>
2023-05-14 02:27:55 +0000

    #11111
    python CONDITIONAL statement with 2 variable 4 parameter && 1/4 output.

---
+++ 4out2para.py
@@ -0,0 +1,11 @@
+#is_Live = True
+#is_Payment = True
+
+if is_Live and is_Payment:
+	print("positive green")
+elif is_Live and not(is_Payment):
+	print("unpaid orange ")
+elif not(is_Live) and is_Payment:
+	print("check orange")
+else:
+	print("negative red")
